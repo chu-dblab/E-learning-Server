1fb94bc31ed1fd66a8c249b59e47eea88b263854
元兒～ <yuan817@moztw.org>
2013-08-02 13:43:19 +0800

    unfinish: 帳號處理檔案

--- htdocs/admin/action/account_process.php
+++ htdocs/admin/action/account_process.php
@@ -1,1 +1,28 @@
- 
+<?php
+/**
+ * 前置設定
+*/
+require_once("../../lib/include.php");
+require_once(DOCUMENT_ROOT."lib/user.php");
+
+/**
+ *
+*/
+$action = $_GET['action'];
+$userid = $_GET['userid'];
+
+if(isset($_SESSION["back_verifyCode"]) && $_SESSION["back_verifyCode"]==$_GET['token']){
+	//驗證成功
+	switch($action){
+		case "update":
+			
+			break;
+		case "delete":
+			
+			break;
+	}
+}
+else{
+	//驗證失敗
+	
+}
