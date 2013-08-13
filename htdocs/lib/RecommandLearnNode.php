1c087aa24ec188d0e99db45088cdaa19dbac5333
TomLin100 <3011850@gmail.com>
2013-08-13 18:50:49 +0800

    Create Recommand Learn Node PHP file

---
+++ htdocs/lib/RecommandLearnNode.php
@@ -0,0 +1,69 @@
+<?php
+   /*
+    *  類別名稱：推薦學習點
+    */
+	class RecommandLearnNode
+	{
+		public function __construct()
+		{
+		}
+		
+		/*
+		 * 方法名稱：加人數
+		 * 說明：當使用者的手機偵測到NFC Tag或掃描到QR code, 則人數加一
+		 */
+		public function addPeople()
+		{
+			
+		}
+		
+		/*
+		 * 方法名稱：減人數
+		 * 說明：當使用者按下"離開"按鈕時, 則人數減一
+		 */
+		
+		public function SubPeople()
+		{
+			
+		}
+		
+		/*
+		 * 方法名稱：isZero
+		 * 說明：確認目前的學習點是不是零
+		 */
+		private function isZero()
+		{
+			
+		}
+		
+		/*
+		 * 方法名稱：getLearningPath
+		 * 說明：取得學習路徑(包含權重值)
+		 */
+		public function getLearningPath()
+		{
+			
+		}
+		
+		
+		public function getNodeOfLearnOfParameter()
+		{
+			
+		}
+		
+		public function getPathOfLearning()
+		{
+			
+		}
+		
+		private function QuickSort()
+		{
+			
+		}
+		
+		public function DetechteIsAllLearnNode()
+		{
+			
+		}
+	}
+?>
