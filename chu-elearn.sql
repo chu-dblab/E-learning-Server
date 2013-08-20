22efa618eef7c3a56fa4c9cbb362378c0ba33261
元兒～ <yuan817@moztw.org>
2013-08-20 15:11:01 +0800

    後端伺服器的所有欄位名稱都去掉chu字首（不然會增加操作SQL的麻煩）

--- chu-elearn.sql
+++ chu-elearn.sql
@@ -27,11 +27,11 @@ SET time_zone = "+00:00";
 -- 
 
 CREATE TABLE `chu_belong` (
-  `chu_TID` int(10) unsigned NOT NULL,
-  `chu_ThemeID` int(10) unsigned NOT NULL,
-  `chu_Weights` float NOT NULL COMMENT '當次學習主題的某一個學習標的之權重',
-  UNIQUE KEY `chu_TID` (`chu_TID`),
-  UNIQUE KEY `chu_ThemeID` (`chu_ThemeID`)
+  `TID` int(10) unsigned NOT NULL,
+  `ThemeID` int(10) unsigned NOT NULL,
+  `Weights` float NOT NULL COMMENT '當次學習主題的某一個學習標的之權重',
+  UNIQUE KEY `TID` (`TID`),
+  UNIQUE KEY `ThemeID` (`ThemeID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 
@@ -46,12 +46,12 @@ CREATE TABLE `chu_belong` (
 -- 
 
 CREATE TABLE `chu_edge` (
-  `chu_Ti` int(10) unsigned NOT NULL,
-  `chu_Tj` int(10) unsigned NOT NULL,
-  `chu_MoveTime` varchar(20) NOT NULL COMMENT '移動時間(分鐘)',
-  `chu_Distance` varchar(20) NOT NULL COMMENT '距離(M)',
-  UNIQUE KEY `chu_Ti` (`chu_Ti`),
-  UNIQUE KEY `chu_Tj` (`chu_Tj`)
+  `Ti` int(10) unsigned NOT NULL,
+  `Tj` int(10) unsigned NOT NULL,
+  `MoveTime` varchar(20) NOT NULL COMMENT '移動時間(分鐘)',
+  `Distance` varchar(20) NOT NULL COMMENT '距離(M)',
+  UNIQUE KEY `Ti` (`Ti`),
+  UNIQUE KEY `Tj` (`Tj`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='標的和標的之間 資料表';
 
 -- 
@@ -66,10 +66,10 @@ CREATE TABLE `chu_edge` (
 -- 
 
 CREATE TABLE `chu_group` (
-  `chu_GID` int(10) unsigned NOT NULL,
-  `chu_GName` varchar(15) NOT NULL,
-  `chu_GCompetence` varchar(10) NOT NULL,
-  PRIMARY KEY  (`chu_GID`)
+  `GID` int(10) unsigned NOT NULL,
+  `GName` varchar(15) NOT NULL,
+  `GCompetence` varchar(10) NOT NULL,
+  PRIMARY KEY  (`GID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 
@@ -84,12 +84,12 @@ CREATE TABLE `chu_group` (
 -- 
 
 CREATE TABLE `chu_question` (
-  `chu_QID` int(10) unsigned NOT NULL,
-  `chu_QA` varchar(10) NOT NULL,
-  `chu_Q_Url` varchar(50) NOT NULL,
-  `chu_TID` int(10) unsigned NOT NULL,
-  PRIMARY KEY  (`chu_QID`),
-  UNIQUE KEY `chu_TID` (`chu_TID`)
+  `QID` int(10) unsigned NOT NULL,
+  `QA` varchar(10) NOT NULL,
+  `Q_Url` varchar(50) NOT NULL,
+  `TID` int(10) unsigned NOT NULL,
+  PRIMARY KEY  (`QID`),
+  UNIQUE KEY `TID` (`TID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 
@@ -104,11 +104,11 @@ CREATE TABLE `chu_question` (
 -- 
 
 CREATE TABLE `chu_recommend` (
-  `chu_TID` int(10) unsigned NOT NULL,
-  `chu_UID` int(10) unsigned NOT NULL,
-  `chu_Order` int(10) unsigned NOT NULL COMMENT '系統推薦標地順序',
-  UNIQUE KEY `chu_TID` (`chu_TID`,`chu_UID`),
-  KEY `chu_UID` (`chu_UID`)
+  `TID` int(10) unsigned NOT NULL,
+  `UID` int(10) unsigned NOT NULL,
+  `Order` int(10) unsigned NOT NULL COMMENT '系統推薦標地順序',
+  UNIQUE KEY `TID` (`TID`,`UID`),
+  KEY `UID` (`UID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 
@@ -123,16 +123,16 @@ CREATE TABLE `chu_recommend` (
 -- 
 
 CREATE TABLE `chu_study` (
-  `chu_TID` int(10) unsigned NOT NULL,
-  `chu_UID` int(10) unsigned NOT NULL,
-  `chu_QID` int(10) unsigned default NULL,
-  `chu_Answer` varchar(5) default NULL COMMENT '答題對錯 Y=對 N=錯',
-  `chu_Answer_Time` varchar(10) default NULL COMMENT '作答時間',
-  `chu_In_TargetTime` datetime NOT NULL,
-  `chu_Out_TargetTime` datetime default NULL,
-  `chu_TCheck` varchar(5) NOT NULL COMMENT '有無正確到推薦點',
-  UNIQUE KEY `TID` (`chu_TID`),
-  UNIQUE KEY `SID` (`chu_UID`)
+  `TID` int(10) unsigned NOT NULL,
+  `UID` int(10) unsigned NOT NULL,
+  `QID` int(10) unsigned default NULL,
+  `Answer` varchar(5) default NULL COMMENT '答題對錯 Y=對 N=錯',
+  `Answer_Time` varchar(10) default NULL COMMENT '作答時間',
+  `In_TargetTime` datetime NOT NULL,
+  `Out_TargetTime` datetime default NULL,
+  `TCheck` varchar(5) NOT NULL COMMENT '有無正確到推薦點',
+  UNIQUE KEY `TID` (`TID`),
+  UNIQUE KEY `SID` (`UID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 
@@ -147,20 +147,20 @@ CREATE TABLE `chu_study` (
 -- 
 
 CREATE TABLE `chu_target` (
-  `chu_TID` int(10) unsigned NOT NULL,
-  `chu_TName` varchar(15) NOT NULL,
-  `chu_TLearn_Time` int(10) unsigned NOT NULL,
-  `chu_MapID` int(10) unsigned NOT NULL,
-  `chu_Map_Url` varchar(50) NOT NULL,
-  `chu_FloorName` varchar(50) NOT NULL,
-  `chu_BlockName` varchar(50) NOT NULL,
-  `chu_BlockMap` varchar(50) NOT NULL,
-  `chu_CourseName` varchar(50) NOT NULL,
-  `chu_MaterialID` int(10) unsigned NOT NULL,
-  `chu_Material_Url` varchar(50) NOT NULL,
-  `chu_PLj` int(11) unsigned NOT NULL COMMENT '學習標的人數限制',
-  `chu_Mj` int(11) unsigned default NULL COMMENT '目前人數',
-  PRIMARY KEY  (`chu_TID`)
+  `TID` int(10) unsigned NOT NULL,
+  `TName` varchar(15) NOT NULL,
+  `TLearn_Time` int(10) unsigned NOT NULL,
+  `MapID` int(10) unsigned NOT NULL,
+  `Map_Url` varchar(50) NOT NULL,
+  `FloorName` varchar(50) NOT NULL,
+  `BlockName` varchar(50) NOT NULL,
+  `BlockMap` varchar(50) NOT NULL,
+  `CourseName` varchar(50) NOT NULL,
+  `MaterialID` int(10) unsigned NOT NULL,
+  `Material_Url` varchar(50) NOT NULL,
+  `PLj` int(11) unsigned NOT NULL COMMENT '學習標的人數限制',
+  `Mj` int(11) unsigned default NULL COMMENT '目前人數',
+  PRIMARY KEY  (`TID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 
@@ -175,12 +175,12 @@ CREATE TABLE `chu_target` (
 -- 
 
 CREATE TABLE `chu_theme` (
-  `chu_ThemeID` int(10) unsigned NOT NULL,
-  `chu_ThemeName` varchar(15) NOT NULL,
-  `chu_theme_Learn_DateTime` datetime NOT NULL COMMENT '本次學習此主題發生的日期時間',
-  `chu_Theme_LearnTotal` int(10) unsigned NOT NULL COMMENT '學習此主題要花的總時間(hr)',
-  `chu_Theme_Introduction` varchar(70) NOT NULL,
-  PRIMARY KEY  (`chu_ThemeID`)
+  `ThemeID` int(10) unsigned NOT NULL,
+  `ThemeName` varchar(15) NOT NULL,
+  `theme_Learn_DateTime` datetime NOT NULL COMMENT '本次學習此主題發生的日期時間',
+  `Theme_LearnTotal` int(10) unsigned NOT NULL COMMENT '學習此主題要花的總時間(hr)',
+  `Theme_Introduction` varchar(70) NOT NULL,
+  PRIMARY KEY  (`ThemeID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 
@@ -195,21 +195,21 @@ CREATE TABLE `chu_theme` (
 -- 
 
 CREATE TABLE `chu_user` (
-  `chu_UID` int(10) unsigned NOT NULL COMMENT 'S#',
-  `chu_UPassword` varchar(30) NOT NULL COMMENT '密碼',
-  `chu_UName` varchar(20) NOT NULL COMMENT '使用者名稱',
-  `chu_UReal_Name` varchar(20) NOT NULL COMMENT '真實名稱',
-  `chu_ULogged_no` varchar(40) default NULL,
-  `chu_UAccount_no` varchar(20) NOT NULL COMMENT '帳號',
-  `chu_ULast_In_Time` datetime default NULL,
-  `chu_UNickname` varchar(20) default NULL COMMENT '使用者暱稱',
-  `chu_UEmail` varchar(50) NOT NULL,
-  `chu_UBuild_Time` datetime NOT NULL COMMENT '帳號建立時間',
-  `chu_UEnabled` varchar(10) NOT NULL COMMENT '帳號啟用狀態',
-  `chu_In_Learn_Time` datetime NOT NULL,
-  `chu_GID` int(10) unsigned NOT NULL,
-  PRIMARY KEY  (`chu_UID`),
-  KEY `GID` (`chu_GID`)
+  `UID` int(10) unsigned NOT NULL COMMENT 'S#',
+  `UPassword` varchar(30) NOT NULL COMMENT '密碼',
+  `UName` varchar(20) NOT NULL COMMENT '使用者名稱',
+  `UReal_Name` varchar(20) NOT NULL COMMENT '真實名稱',
+  `ULogged_no` varchar(40) default NULL,
+  `UAccount_no` varchar(20) NOT NULL COMMENT '帳號',
+  `ULast_In_Time` datetime default NULL,
+  `UNickname` varchar(20) default NULL COMMENT '使用者暱稱',
+  `UEmail` varchar(50) NOT NULL,
+  `UBuild_Time` datetime NOT NULL COMMENT '帳號建立時間',
+  `UEnabled` varchar(10) NOT NULL COMMENT '帳號啟用狀態',
+  `In_Learn_Time` datetime NOT NULL,
+  `GID` int(10) unsigned NOT NULL,
+  PRIMARY KEY  (`UID`),
+  KEY `GID` (`GID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 
@@ -225,38 +225,38 @@ CREATE TABLE `chu_user` (
 -- 資料表限制 `chu_belong`
 -- 
 ALTER TABLE `chu_belong`
-  ADD CONSTRAINT `chu_belong_ibfk_2` FOREIGN KEY (`chu_ThemeID`) REFERENCES `chu_theme` (`chu_ThemeID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `chu_belong_ibfk_1` FOREIGN KEY (`chu_TID`) REFERENCES `chu_target` (`chu_TID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `chu_belong_ibfk_2` FOREIGN KEY (`ThemeID`) REFERENCES `chu_theme` (`ThemeID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `chu_belong_ibfk_1` FOREIGN KEY (`TID`) REFERENCES `chu_target` (`TID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- 
 -- 資料表限制 `chu_edge`
 -- 
 ALTER TABLE `chu_edge`
-  ADD CONSTRAINT `chu_edge_ibfk_2` FOREIGN KEY (`chu_Tj`) REFERENCES `chu_target` (`chu_TID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `chu_edge_ibfk_1` FOREIGN KEY (`chu_Ti`) REFERENCES `chu_target` (`chu_TID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `chu_edge_ibfk_2` FOREIGN KEY (`Tj`) REFERENCES `chu_target` (`TID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `chu_edge_ibfk_1` FOREIGN KEY (`Ti`) REFERENCES `chu_target` (`TID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- 
 -- 資料表限制 `chu_question`
 -- 
 ALTER TABLE `chu_question`
-  ADD CONSTRAINT `chu_question_ibfk_1` FOREIGN KEY (`chu_TID`) REFERENCES `chu_target` (`chu_TID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `chu_question_ibfk_1` FOREIGN KEY (`TID`) REFERENCES `chu_target` (`TID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- 
 -- 資料表限制 `chu_recommend`
 -- 
 ALTER TABLE `chu_recommend`
-  ADD CONSTRAINT `chu_recommend_ibfk_2` FOREIGN KEY (`chu_UID`) REFERENCES `chu_user` (`chu_UID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `chu_recommend_ibfk_1` FOREIGN KEY (`chu_TID`) REFERENCES `chu_target` (`chu_TID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `chu_recommend_ibfk_2` FOREIGN KEY (`UID`) REFERENCES `chu_user` (`UID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `chu_recommend_ibfk_1` FOREIGN KEY (`TID`) REFERENCES `chu_target` (`TID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- 
 -- 資料表限制 `chu_study`
 -- 
 ALTER TABLE `chu_study`
-  ADD CONSTRAINT `chu_study_ibfk_2` FOREIGN KEY (`chu_UID`) REFERENCES `chu_user` (`chu_UID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `chu_study_ibfk_1` FOREIGN KEY (`chu_TID`) REFERENCES `chu_target` (`chu_TID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `chu_study_ibfk_2` FOREIGN KEY (`UID`) REFERENCES `chu_user` (`UID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `chu_study_ibfk_1` FOREIGN KEY (`TID`) REFERENCES `chu_target` (`TID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- 
 -- 資料表限制 `chu_user`
 -- 
 ALTER TABLE `chu_user`
-  ADD CONSTRAINT `chu_user_ibfk_1` FOREIGN KEY (`chu_GID`) REFERENCES `chu_group` (`chu_GID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `chu_user_ibfk_1` FOREIGN KEY (`GID`) REFERENCES `chu_group` (`GID`) ON DELETE CASCADE ON UPDATE CASCADE;
