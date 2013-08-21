42b42bdc6e7721597d0b807e841406081903c361
元兒～ <yuan817@moztw.org>
2013-08-21 13:53:00 +0800

    使用者管理網頁更換成模組化的通知

--- htdocs/admin/user_list.php
+++ htdocs/admin/user_list.php
@@ -14,37 +14,18 @@
 	require_once("../lib/include.php");
 	require_once(DOCUMENT_ROOT."admin/template/template.php");
 	require_once(DOCUMENT_ROOT."lib/user.php");
+	require_once(DOCUMENT_ROOT."lib/web/AlertClass.php");
 	
-	//讀取session資料
-	session_start();
-	if( isset($_SESSION["user_create_status"]) ) {
-		$status_create =  $_SESSION["user_create_status"];
-	}
-	if( isset($_SESSION["user_create_status_message"]) ) {
-		$status_create_message =  $_SESSION["user_create_status_message"];
-	}
-	unset($_SESSION["user_create_status"]);
-	unset($_SESSION["user_create_status_message"]);
+	//取得通知資料
+	$theAlert = new Alert();
+	$theAlert->getInSession("user_create");
+	
 	// ------------------------------------------------------------------------
 	
-	//取得上個頁面傳來的訊息
+	//輸出通知資料
 	function show_status_notify(){
-		global $status_create, $status_create_message;
-		
-		if($status_create){
-			echo "<div class='alert";
-			switch($status_create){
-				case "Finish":
-					echo " alert-success";
-					break;
-			}
-			echo "'>";
-			
-			echo "<button type='button' class='close' data-dismiss='alert'>&times;</button>";
-			echo $status_create_message;
-			
-			echo "</div>";
-		}
+		global $theAlert;
+		$theAlert->show();
 	}
 	
 	//取得共有幾個使用者
