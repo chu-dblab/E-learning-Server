0db76852d9cfa3e0f6b76f6fe6fecc33001218c5
~kobayashi() <3011850@gmail.com>
2013-08-24 19:10:16 +0800

    add isZero() method imprelement, but not compelet yet.

--- htdocs/lib/RecommandLearnNode.php
+++ htdocs/lib/RecommandLearnNode.php
@@ -1,48 +1,56 @@
 <?php
-   /*
-    *  類別名稱：推薦學習點
-    */
+ 	require_once("lib/include.php");
+    require_once(DOCUMENT_ROOT."lib/DatabaseClass.php");
 	
-	require_once("DatabaseClass.php");
+	/*
+	 *  類別名稱：推薦學習點
+     */
 	class RecommandLearnNode
 	{
 		private $conDB;
 		public function __construct()
 		{
-		  $conDB = new DatabaseClass();
+		  $conDB = new Database();
 		}
 		
 		/*
 		 * 方法名稱：加人數
 		 * 說明：當使用者的手機偵測到NFC Tag或掃描到QR code, 則人數加一
-		 * 參數: $number, 學習點的編號
+		 * 參數: $point_number, 學習點的編號
 		 * 回傳值： NONE
 		 */
-		public function addPeople($number)
+		public function addPeople($point_number
 		{
-		    $query = "UPDATE target set Mj = Mj + 1 where number = ".number;
-		    $result = $conDB->execute($query);		    
+		    $query = $conDB->prepare("UPDATE ".$conDB->table("target")." Mj = Mj + 1 where TID = :number");
+			$query->bindParam(":number",$point_number);
+		    $result->execute();		    
 		}
 		
 		/*
 		 * 方法名稱：減人數
 		 * 說明：當使用者按下"離開"按鈕時, 則人數減一
-		 * 參數: $number, 學習點的編號
+		 * 參數: $point_number, 學習點的編號
 		 * 回傳值： NONE
 		 */		
-		public function SubPeople($number)
+		public function subPeople($point_number)
 		{
-		     $query = "UPDATE target set Mj = Mj - 1 where number = ".number;
-		     $result = $conDB->execute($query);	
+		    $query = $conDB->prepare("UPDATE ".$conDB->table("target")." set Mj = Mj - 1 where TID = :number");
+			$query->bindParam(":number",$point_number);
+		    $result->execute();		    
 		}
 		
-		/*
+		/* TODO:
 		 * 方法名稱：isZero
 		 * 說明：確認目前的學習點是不是零
+		 * 參數：point_number (data type is an Integer)  學習點的編號
+		 * 回傳值：Boolean (true/false)
 		 */
-		private function isZero()
+		private function isZero($point_number)
 		{
-			
+			$isZero = false;
+			$result = $conDB->prepare("SELECT Mj FROM ".$conDB->table("target")." WHERE TID = :number AND Mj = 0");
+			$result->bindParam(":number",$point_number);
+			$result->excute();
 		}
 		
 		/*
@@ -59,15 +67,6 @@
 		 * 說明：取得學習點的所有參數
 		 */
 		public function getNodeOfLearnOfParameter()
-		{
-			
-		}
-		
-		/*
-		 * 方法名稱：QuickSort
-		 * 說明：隊取得的資料作快速排序
-		 */
-		private function QuickSort()
 		{
 			
 		}
