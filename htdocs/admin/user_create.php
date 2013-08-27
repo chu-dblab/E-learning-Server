f270c655867d7b47ec71721437240b5caaa8feb6
元兒～ <yuan817@moztw.org>
2013-08-27 11:07:02 +0800

    fix user_crate require_once url error

--- htdocs/admin/user_create.php
+++ htdocs/admin/user_create.php
@@ -14,7 +14,7 @@
 	require_once("../lib/include.php");
 	require_once(DOCUMENT_ROOT."lib/function/user.php");
 	require_once(DOCUMENT_ROOT."lib/function/userGroup.php");
-	require_once(DOCUMENT_ROOT."admin/templacte/template.php");
+	require_once(DOCUMENT_ROOT."admin/template/template.php");
 	require_once(DOCUMENT_ROOT."lib/web/AlertClass.php");
 	
 	//取得通知資料
