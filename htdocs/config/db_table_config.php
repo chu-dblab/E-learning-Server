a96ce9eaee3ab3f00ff75a20f9e2075a6782847f
元兒～ <yuan817@moztw.org>
2013-08-27 21:49:41 +0800

    Update db_table_config.php

--- htdocs/config/db_table_config.php
+++ htdocs/config/db_table_config.php
@@ -1,3 +1,10 @@
 <?php 
 $FORM_USER = "user";	//使用者帳號資料表
 $FORM_USER_GROUP = "group";	//使用者帳號資料表
+$FORM_BELONG = "belong";
+$FORM_EDGE = "edge";
+$FORM_QUESTION = "question";
+$FORM_RECOMMEND = "recommend";
+$FORM_STUDY = "study";
+$FORM_TARGET = "target";
+$FORM_THEME = "theme";
