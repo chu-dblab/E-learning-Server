c7f3bb17f5a6483eb70e659a9774893434ebe1d1
元兒～ <yuan817@moztw.org>
2013-08-27 21:50:32 +0800

    add database function: add db_rename_prefix

---
+++ htdocs/lib/function/database.php
@@ -0,0 +1,34 @@
+<?php
+require_once(DOCUMENT_ROOT."lib/class/Database.php");
+require_once(DOCUMENT_ROOT."config/db_config.php"); //取得連結資料庫連結變數
+require_once(DOCUMENT_ROOT."config/db_table_config.php");
+require_once(DOCUMENT_ROOT."lib/create_txt/create_db_config.php");
+
+function db_rename_prefix($newPrefix) {
+	global $FORM_PREFIX;
+	global $FORM_USER, $FORM_USER_GROUP, $FORM_BELONG, $FORM_EDGE, $FORM_QUESTION, $FORM_RECOMMEND, $FORM_STUDY, $FORM_TARGET, $FORM_THEME;
+	
+	//更改所有資料表名稱的前綴字元
+	$db = new Database();
+	$db->exec("RENAME TABLE `".$FORM_PREFIX.$FORM_USER."` TO `".$newPrefix.$FORM_USER."`");
+	$db->exec("RENAME TABLE `".$FORM_PREFIX.$FORM_USER_GROUP."` TO `".$newPrefix.$FORM_USER_GROUP."`");
+	$db->exec("RENAME TABLE `".$FORM_PREFIX.$FORM_BELONG."` TO `".$newPrefix.$FORM_BELONG."`");
+	$db->exec("RENAME TABLE `".$FORM_PREFIX.$FORM_EDGE."` TO `".$newPrefix.$FORM_EDGE."`");
+	$db->exec("RENAME TABLE `".$FORM_PREFIX.$FORM_QUESTION."` TO `".$newPrefix.$FORM_QUESTION."`");
+	$db->exec("RENAME TABLE `".$FORM_PREFIX.$FORM_RECOMMEND."` TO `".$newPrefix.$FORM_RECOMMEND."`");
+	$db->exec("RENAME TABLE `".$FORM_PREFIX.$FORM_STUDY."` TO `".$newPrefix.$FORM_STUDY."`");
+	$db->exec("RENAME TABLE `".$FORM_PREFIX.$FORM_TARGET."` TO `".$newPrefix.$FORM_TARGET."`");
+	$db->exec("RENAME TABLE `".$FORM_PREFIX.$FORM_THEME."` TO `".$newPrefix.$FORM_THEME."`");
+	
+	//寫入新的設定檔
+	global $DB_SERV, $DB_USER, $DB_PASS, $DB_NAME;
+	$create_txt_content = create_config_txt_content($DB_SERV, $DB_USER, $DB_PASS, $DB_NAME, $newPrefix);
+	if($fp=fopen(DOCUMENT_ROOT."config/db_config.php","w+")){
+		fputs($fp,$create_txt_content);
+		return "Finish";
+	}
+	else{
+		return $create_txt_content;
+	}
+	fclose($fp);
+}
