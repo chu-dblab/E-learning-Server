9b3e9aa466bde4de3439637519f0bea9f95dc9ea
~kobayashi() <3011850@gmail.com>
2013-08-28 00:57:58 +0800

    fix include path

--- htdocs/api/addPeople.php
+++ htdocs/api/addPeople.php
@@ -1,5 +1,5 @@
 <?php
-  require_once(DOCUMENT_ROOT."lib/include.php");
+  require_once("../lib/include.php");
   require_once(DOCUMENT_ROOT."lib/class/RecommandLearnNode.php");
   
   $num = $_POST["numberOfPeople"];
