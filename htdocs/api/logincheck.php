5f91d5f348b8e13bb30d4ff075f08d428c59604e
元兒～ <yuan817@moztw.org>
2013-08-28 00:53:29 +0800

    fix api/logincheck.php

--- htdocs/api/logincheck.php
+++ htdocs/api/logincheck.php
@@ -2,7 +2,7 @@
 	/*
 	 * Android Phone can access this file to get JSON format message.
 	 */
-	require_once(DOCUMENT_ROOT."/lib/include.php");
+	require_once("../lib/include.php");
 	require_once(DOCUMENT_ROOT."/lib/function/user.php");
 	$ID = $_POST["mId"];
 	$PWD = $_POST["mPassword"];
