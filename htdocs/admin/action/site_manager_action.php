68551c60bf8fb0c15b1d9c722908a17b79bba076
元兒～ <yuan817@moztw.org>
2013-09-03 19:13:01 +0800

    Add site_manager action

---
+++ htdocs/admin/action/site_manager_action.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 前置設定
+*/
+require_once("../../lib/include.php");
+require_once(DOCUMENT_ROOT."lib/function/user.php");
+
+/**
+ *
+*/
+$action = $_GET['action'];
+
+switch($action){
+	case "":
+	
+}
