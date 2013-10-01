48f07aa49356f3bcd491043ab6cf1fe5697c8129
元兒～ <yuan817@moztw.org>
2013-10-01 15:48:52 +0800

    api規劃: 帳號

--- htdocs/api/login.php
+++ htdocs/api/login.php
@@ -3,19 +3,20 @@ require_once("../lib/include.php");
 require_once(DOCUMENT_ROOT."/lib/function/user.php");
 require_once(DOCUMENT_ROOT."/lib/class/MyUser.php");
 
-$action = $_REQUEST['action'];
+//-------------------設定區-----------------------//
+$action=(empty($_REQUEST['op']))?"":$_REQUEST['op'];
 
+//---------------流程控制區----------------------//
 switch($action){
-
 case "login":
-	$ID = $_POST["mId"];
-	$PWD = $_POST["mPassword"];
+	$ID = $_REQUEST["uid"];
+	$PWD = $_REQUEST["upasswd"];
 	$login_code = array(code=>user_login($ID,$PWD));
 	echo json_encode($login_code);
 	break;
 
 case "logout":
-	$logCode = $_POST["mCode"];
+	$logCode = $_REQUEST["ucode"];
 	$user = new MyUser($logCode);
 	$user->logout();
 	break;
