55fcf7222991ed2b3c955eddb7963706fccd4a27
~kobayashi() <3011850@gmail.com>
2013-10-24 00:19:48 +0800

    完成架構修改

--- htdocs/API/v1/Learn/people.php
+++ htdocs/API/v1/Learn/people.php
@@ -1,3 +1,23 @@
 <?php
-
+     require_once("../lib/include.php");
+     require_once(DOCUMENT_ROOT."lib/class/RecommandLearnNode.php");
+     
+     $action = (empty($_REQUEST["op"])) ? null : $_REQUEST["op"];
+     $num = (empty($_REQUEST["amount"])) ? null : $_REQUEST["amount"];
+     $Data = (empty($_REQUEST["data"])) ? null : $_REQUEST["data"];
+     $add = new RecommandLearnNode();
+     
+     switch($action)
+     {
+	case "addPeople":
+	    $add->addPeople($num);
+	    break;
+	case "subPeople":
+	    $subpeople->subPeople($num);
+	    break;
+	case "upgrade":
+	     $update->updateUserLearnData($Data);
+	    break;	   
+     }
+     
 ?>
