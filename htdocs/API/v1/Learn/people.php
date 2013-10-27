e5ca5e2c366b391d63f3ff20f2ea8859c4f775c4
~kobayashi() <3011850@gmail.com>
2013-10-28 04:52:08 +0800

    新增錯誤訊息的JSON格式輸出

--- htdocs/API/v1/Learn/people.php
+++ htdocs/API/v1/Learn/people.php
@@ -4,39 +4,52 @@
      require_once(DOCUMENT_ROOT."lib/class/RecommandLearnNode.php");
      
      $action = (empty($_REQUEST["op"])) ? null : $_REQUEST["op"];
-     $num = (empty($_REQUEST["amount"])) ? null : $_REQUEST["amount"];
-     $Data = (empty($_REQUEST["data"])) ? null : $_REQUEST["data"];
+     $num = (empty($_POST["amount"])) ? null : $_POST["amount"];
+     $Data = (empty($_POST["data"])) ? null : $_POST["data"];
      $point = (empty($_REQUEST["point"])) ? null : $_REQUEST["point"];
      $ID = (empty($_REQUEST["uid"])) ? null : $_REQUEST["uid"];
      $learn = new RecommandLearnNode();
      
      $message = array();
      
-    // if(isset($action) && isset($num))
-     
      switch($action)
      {
 	case "addPeople":
-	    $learn->addPeople($num);
-	    $message += array("status"=>"OK");
+	    if(!isset($num)) $message += array("status"=>"CmdErr");
+	    else
+	    {
+		$learn->addPeople($num);
+		$message += array("status"=>"OK");
+	    }
 	    break;
 	case "subPeople":
-	    $learn->subPeople($num);
-	     $message += array("status"=>"OK");
+	    if(!isset($num)) $message += array("status"=>"CmdErr");
+	    else 
+	    {
+		$learn->subPeople($num);
+		$message += array("status"=>"OK");
+	    }
 	    break;
 	case "upgrade":
-	     $learn->updateUserLearnData($Data);
-	     $message += array("status"=>"OK");
-	    break;
+	     if(!isset($Data)) $message += array("status"=>"CmdErr");
+	     else 
+	     {
+		$learn->updateUserLearnData($Data);
+		$message += array("status"=>"OK");
+	     }
+	     break;
 	case "recommand" :
-	      $learn->getLearningNode($point,$ID);
-	      $message += array("status"=>"OK");
+	      if(isset($point) && isset($ID))
+	      {
+		  $learn->getLearningNode($point,$ID);
+		  $message += array("status"=>"OK");
+	      }
+	      else $message += array("status"=>"CmdErr");
 	    break;
 	default:
-	      $message += array("status"=>"ERROR!!");
+	      $message += array("status"=>"Internal Error!!");
      }
      
      apitemplate_header();
-     echo json_encode($message);
-     
+     echo json_encode($message);     
 ?>
