d4154eb1211b1bc7bf6cf721800b14d7f46f24c3
JennyTsai <lupjk123@gmail.com>
2013-11-01 01:47:48 +0800

    SQL檔最新更正,確定可以執行！

--- chu-elearn.sql
+++ chu-elearn.sql
@@ -1,259 +1,105 @@
--- phpMyAdmin SQL Dump
--- version 2.10.3
--- http://www.phpmyadmin.net
--- 
--- 主機: localhost
--- 建立日期: Sep 16, 2013, 10:43 AM
--- 伺服器版本: 5.0.51
--- PHP 版本: 5.2.6
+﻿CREATE TABLE chu_group(
+  GID varchar(30) NOT NULL,
+  GName varchar(15) NOT NULL,
+  Gauth_admin tinyint(1) NOT NULL default '0',
+  GCompetence varchar(10) NOT NULL,
+  PRIMARY KEY (GID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='群組 資料表';
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+INSERT
+INTO chu_group
+VALUES ('admin', '管理者', 1, '');
 
--- 
--- 資料庫: `chu-elearn`
--- 
+INSERT
+INTO chu_group
+VALUES ('user', '使用者', 0, '');
 
--- --------------------------------------------------------
+CREATE TABLE chu_user(
+  SID varchar(30) NOT NULL COMMENT '使用者帳號',
+  GID varchar(30) NOT NULL COMMENT '使用者群組',
+  UPassword varchar(40) NOT NULL COMMENT '密碼',
+  ULogged_code varchar(32) default NULL COMMENT '登入碼',
+  ULast_In_Time timestamp NULL default NULL COMMENT '最後登入時間',
+  UBuild_Time timestamp NOT NULL default CURRENT_TIMESTAMP COMMENT '帳號建立時間',
+  UEnabled tinyint(1) NOT NULL default '1' COMMENT '帳號啟用狀態',
+  In_Learn_Time datetime NOT NULL COMMENT '開始學習時間',
+  UReal_Name varchar(20) default NULL COMMENT '真實姓名',
+  UNickname varchar(20) default NULL COMMENT '使用者暱稱',
+  UEmail varchar(50) default NULL COMMENT '使用者email',
+  PRIMARY KEY (SID),
+  FOREIGN KEY (GID) REFERENCES chu_group (GID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='使用者 資料表';
 
--- 
--- 資料表格式： `chu_belong`
--- 
+INSERT
+INTO chu_user
+VALUES ('root', 'admin', '63a9f0ea7bb98050796b649e854818', NULL, NULL, '2013-08-20 14:58:38', 1, '0000-00-00 00:00:00', '', '', '');
 
-CREATE TABLE `chu_belong` (
-  `TID` int(10) unsigned NOT NULL,
-  `ThemeID` int(10) unsigned NOT NULL,
-  `Weights` float NOT NULL COMMENT '當次學習主題的某一個學習標的之權重',
-  UNIQUE KEY `TID` (`TID`),
-  UNIQUE KEY `ThemeID` (`ThemeID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE chu_target(
+  TID int(10) unsigned NOT NULL,
+  TName varchar(15) NOT NULL,
+  TLearn_Time int(10) unsigned NOT NULL,
+  MapID int(10) unsigned NOT NULL,
+  Map_Url varchar(150) NOT NULL,
+  FloorName varchar(50) NOT NULL,
+  BlockName varchar(50) NOT NULL,
+  BlockMap varchar(50) NOT NULL,
+  CourseName varchar(50) NOT NULL,
+  MaterialID int(10) unsigned NOT NULL,
+  Material_Url varchar(150) NOT NULL,
+  PLj int(200) unsigned NOT NULL COMMENT '學習標的人數限制',
+  Mj int(200) unsigned default NULL COMMENT '目前人數',
+  S float unsigned default NULL COMMENT '學習標的飽和率上限',
+  Fj tinyint(1) default NULL COMMENT '學習標的滿額指標',
+  PRIMARY KEY (TID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='標的 資料表';
 
--- 
--- 列出以下資料庫的數據： `chu_belong`
--- 
+CREATE TABLE chu_theme(
+  ThemeID int(10) unsigned NOT NULL,
+  ThemeName varchar(15) NOT NULL,
+  theme_Learn_DateTime datetime NOT NULL COMMENT '本次學習此主題發生的日期時間',
+  Theme_LearnTotal int(10) unsigned NOT NULL COMMENT '學習此主題要花的總時間(hr)',
+  Theme_Introduction varchar(70) NOT NULL,
+  PRIMARY KEY (ThemeID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='主題 資料表';
 
+CREATE TABLE chu_study(
+  TID int(10) unsigned NOT NULL,
+  SID varchar(30) NOT NULL COMMENT '使用者帳號',
+  QID int(10) unsigned default NULL,
+  TCheck varchar(5) NOT NULL COMMENT '有無正確到推薦點',
+  Answer varchar(5) default NULL COMMENT '答題對錯 Y=對 N=錯',
+  Answer_Time varchar(10) default NULL COMMENT '作答時間',
+  In_TargetTime datetime NOT NULL,
+  Out_TargetTime datetime default NULL,
+  PRIMARY KEY (TID,SID),
+  FOREIGN KEY (TID) REFERENCES chu_target (TID),
+  FOREIGN KEY (SID) REFERENCES chu_user (SID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='使用者與標的間study 資料表';
 
--- --------------------------------------------------------
+CREATE TABLE chu_recommend(
+  TID int(10) unsigned NOT NULL,
+  SID varchar(30) NOT NULL COMMENT '使用者帳號',
+  gradation int(50) unsigned NOT NULL COMMENT '系統推薦標地順序',
+  PRIMARY KEY (TID,SID),
+  FOREIGN KEY (TID) REFERENCES chu_target (TID),
+  FOREIGN KEY (SID) REFERENCES chu_user (SID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='推薦 資料表';
 
--- 
--- 資料表格式： `chu_edge`
--- 
+CREATE TABLE chu_belong(
+  TID int(10) unsigned NOT NULL,
+  ThemeID int(10) unsigned NOT NULL,
+  Weights float NOT NULL COMMENT '當次學習主題的某一個學習標的之權重',
+  PRIMARY KEY (TID,ThemeID),
+  FOREIGN KEY (TID) REFERENCES chu_target (TID),
+  FOREIGN KEY (ThemeID) REFERENCES chu_theme(ThemeID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='標的和主題之間 資料表';
 
-CREATE TABLE `chu_edge` (
-  `Ti` int(10) unsigned NOT NULL,
-  `Tj` int(10) unsigned NOT NULL,
-  `MoveTime` varchar(20) NOT NULL COMMENT '移動時間(分鐘)',
-  `Distance` varchar(20) NOT NULL COMMENT '距離(M)',
-  UNIQUE KEY `Ti` (`Ti`),
-  UNIQUE KEY `Tj` (`Tj`)
+CREATE TABLE chu_edge(
+  Ti int(10) unsigned NOT NULL,
+  Tj int(10) unsigned NOT NULL,
+  MoveTime varchar(20) NOT NULL COMMENT '移動時間(分鐘)',
+  Distance varchar(20) NOT NULL COMMENT '距離(M)',
+  PRIMARY KEY (Ti,Tj),
+  FOREIGN KEY (Ti) REFERENCES chu_target (TID),
+  FOREIGN KEY (Tj) REFERENCES chu_target (TID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='標的和標的之間 資料表';
-
--- 
--- 列出以下資料庫的數據： `chu_edge`
--- 
-
-
--- --------------------------------------------------------
-
--- 
--- 資料表格式： `chu_group`
--- 
-
-CREATE TABLE `chu_group` (
-  `GID` varchar(30) NOT NULL,
-  `GName` varchar(15) NOT NULL,
-  `Gauth_admin` tinyint(1) NOT NULL default '0',
-  `GCompetence` varchar(10) NOT NULL,
-  PRIMARY KEY  (`GID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- 
--- 列出以下資料庫的數據： `chu_group`
--- 
-
-INSERT INTO `chu_group` VALUES ('admin', '管理者', 1, '');
-INSERT INTO `chu_group` VALUES ('user', '使用者', 0, '');
-
--- --------------------------------------------------------
-
--- 
--- 資料表格式： `chu_question`
--- 
-
-CREATE TABLE `chu_question` (
-  `QID` int(10) unsigned NOT NULL,
-  `QA` varchar(10) NOT NULL,
-  `Q_Url` varchar(150) NOT NULL,
-  `TID` int(10) unsigned NOT NULL,
-  PRIMARY KEY  (`QID`),
-  UNIQUE KEY `TID` (`TID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- 
--- 列出以下資料庫的數據： `chu_question`
--- 
-
-
--- --------------------------------------------------------
-
--- 
--- 資料表格式： `chu_recommend`
--- 
-
-CREATE TABLE `chu_recommend` (
-  `TID` int(10) unsigned NOT NULL,
-  `UID` varchar(30) NOT NULL COMMENT 'S#',
-  `Order` int(50) unsigned NOT NULL COMMENT '系統推薦標地順序',
-  UNIQUE KEY `TID` (`TID`,`UID`),
-  KEY `UID` (`UID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- 
--- 列出以下資料庫的數據： `chu_recommend`
--- 
-
-
--- --------------------------------------------------------
-
--- 
--- 資料表格式： `chu_study`
--- 
-
-CREATE TABLE `chu_study` (
-  `TID` int(10) unsigned NOT NULL,
-  `UID` varchar(30) NOT NULL COMMENT 'S#',
-  `QID` int(10) unsigned default NULL,
-  `Answer` varchar(5) default NULL COMMENT '答題對錯 Y=對 N=錯',
-  `Answer_Time` varchar(10) default NULL COMMENT '作答時間',
-  `In_TargetTime` datetime NOT NULL,
-  `Out_TargetTime` datetime default NULL,
-  `TCheck` varchar(5) NOT NULL COMMENT '有無正確到推薦點',
-  UNIQUE KEY `TID` (`TID`),
-  UNIQUE KEY `SID` (`UID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- 
--- 列出以下資料庫的數據： `chu_study`
--- 
-
-
--- --------------------------------------------------------
-
--- 
--- 資料表格式： `chu_target`
--- 
-
-CREATE TABLE `chu_target` (
-  `TID` int(10) unsigned NOT NULL,
-  `TName` varchar(15) NOT NULL,
-  `TLearn_Time` int(10) unsigned NOT NULL,
-  `MapID` int(10) unsigned NOT NULL,
-  `Map_Url` varchar(150) NOT NULL,
-  `FloorName` varchar(50) NOT NULL,
-  `BlockName` varchar(50) NOT NULL,
-  `BlockMap` varchar(50) NOT NULL,
-  `CourseName` varchar(50) NOT NULL,
-  `MaterialID` int(10) unsigned NOT NULL,
-  `Material_Url` varchar(150) NOT NULL,
-  `PLj` int(200) unsigned NOT NULL COMMENT '學習標的人數限制',
-  `Mj` int(200) unsigned default NULL COMMENT '目前人數',
-  `S` float unsigned default NOT NULL COMMENT '學習標的飽和率上限',
-  `Fj` tinyint(1) default NOT NULL COMMENT '學習標的滿額指標',
-  PRIMARY KEY  (`TID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- 
--- 列出以下資料庫的數據： `chu_target`
--- 
-
-
--- --------------------------------------------------------
-
--- 
--- 資料表格式： `chu_theme`
--- 
-
-CREATE TABLE `chu_theme` (
-  `ThemeID` int(10) unsigned NOT NULL,
-  `ThemeName` varchar(15) NOT NULL,
-  `theme_Learn_DateTime` datetime NOT NULL COMMENT '本次學習此主題發生的日期時間',
-  `Theme_LearnTotal` int(10) unsigned NOT NULL COMMENT '學習此主題要花的總時間(hr)',
-  `Theme_Introduction` varchar(70) NOT NULL,
-  PRIMARY KEY  (`ThemeID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- 
--- 列出以下資料庫的數據： `chu_theme`
--- 
-
-
--- --------------------------------------------------------
-
--- 
--- 資料表格式： `chu_user`
--- 
-
-CREATE TABLE `chu_user` (
-  `UID` varchar(30) NOT NULL COMMENT 'S#',
-  `GID` varchar(30) NOT NULL COMMENT '使用者群組',
-  `UPassword` varchar(40) NOT NULL COMMENT '密碼',
-  `ULogged_code` varchar(32) default NULL COMMENT '登入碼',
-  `ULast_In_Time` timestamp NULL default NULL COMMENT '最後登入時間',
-  `UBuild_Time` timestamp NOT NULL default CURRENT_TIMESTAMP COMMENT '帳號建立時間',
-  `UEnabled` tinyint(1) NOT NULL default '1' COMMENT '帳號啟用狀態',
-  `In_Learn_Time` datetime NOT NULL COMMENT '開始學習時間',
-  `UReal_Name` varchar(20) default NULL COMMENT '真實姓名',
-  `UNickname` varchar(20) default NULL COMMENT '使用者暱稱',
-  `UEmail` varchar(50) default NULL COMMENT '使用者email',
-  PRIMARY KEY  (`UID`),
-  KEY `GID` (`GID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- 
--- 列出以下資料庫的數據： `chu_user`
--- 
-
-INSERT INTO `chu_user` VALUES ('root', 'admin', '63a9f0ea7bb98050796b649e854818', NULL, NULL, '2013-08-20 14:58:38', 1, '0000-00-00 00:00:00', '', '', '');
-
--- 
--- 備份資料表限制
--- 
-
--- 
--- 資料表限制 `chu_belong`
--- 
-ALTER TABLE `chu_belong`
-  ADD CONSTRAINT `chu_belong_ibfk_2` FOREIGN KEY (`ThemeID`) REFERENCES `chu_theme` (`ThemeID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `chu_belong_ibfk_1` FOREIGN KEY (`TID`) REFERENCES `chu_target` (`TID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- 
--- 資料表限制 `chu_edge`
--- 
-ALTER TABLE `chu_edge`
-  ADD CONSTRAINT `chu_edge_ibfk_2` FOREIGN KEY (`Tj`) REFERENCES `chu_target` (`TID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `chu_edge_ibfk_1` FOREIGN KEY (`Ti`) REFERENCES `chu_target` (`TID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- 
--- 資料表限制 `chu_question`
--- 
-ALTER TABLE `chu_question`
-  ADD CONSTRAINT `chu_question_ibfk_1` FOREIGN KEY (`TID`) REFERENCES `chu_target` (`TID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- 
--- 資料表限制 `chu_recommend`
--- 
-ALTER TABLE `chu_recommend`
-  ADD CONSTRAINT `chu_recommend_ibfk_2` FOREIGN KEY (`UID`) REFERENCES `chu_user` (`UID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `chu_recommend_ibfk_1` FOREIGN KEY (`TID`) REFERENCES `chu_target` (`TID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- 
--- 資料表限制 `chu_study`
--- 
-ALTER TABLE `chu_study`
-  ADD CONSTRAINT `chu_study_ibfk_2` FOREIGN KEY (`UID`) REFERENCES `chu_user` (`UID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `chu_study_ibfk_1` FOREIGN KEY (`TID`) REFERENCES `chu_target` (`TID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- 
--- 資料表限制 `chu_user`
--- 
-ALTER TABLE `chu_user`
-  ADD CONSTRAINT `chu_user_ibfk_1` FOREIGN KEY (`GID`) REFERENCES `chu_group` (`GID`) ON DELETE CASCADE ON UPDATE CASCADE;
