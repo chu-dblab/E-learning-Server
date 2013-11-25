856bcd73a0d0e5893e11549238d1e0e81a913955
元兒～ <yuan817@moztw.org>
2013-11-25 20:37:36 +0800

    fix htdocs/API/v1/Material/DownloadZip.php

--- htdocs/API/v1/Material/DownloadZip.php
+++ htdocs/API/v1/Material/DownloadZip.php
@@ -7,6 +7,10 @@ $the_folder = DOCUMENT_ROOT."Material";
 //$zip_file_name = time().".zip"; // Zip name
 $zip_file_name = "TeachingMaterial.zip"; // Zip name
 
+$requestURLArray = explode("/",$_SERVER['REQUEST_URI']);
+$requestLastURL = explode("?",end($requestURLArray));
+$requestFileName = $requestLastURL[0];
+
 if(extension_loaded('zip'))
 {
 	$zip = new FlxZipArchive;
@@ -20,7 +24,7 @@ if(extension_loaded('zip'))
 			// push to download the zip
 			header('Content-type: application/zip');
 			
-			header('Content-Disposition: attachment; filename="'.str_replace(".php",".zip",basename(__FILE__)).'"');
+			header('Content-Disposition: attachment; filename="'.str_replace(".php",".zip", $requestFileName ).'"');
 			header('Content-Length: ' . filesize($zip_file_name));
 			readfile($zip_file_name);
 			// remove zip file is exists in temp path
