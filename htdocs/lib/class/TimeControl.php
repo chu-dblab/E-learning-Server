686364de96aeda1a792c2d6817ec224e3450a05a
~kobayashi() <3011850@gmail.com>
2014-01-17 17:26:49 +0800

    新增取得學習總時間方法

--- htdocs/lib/class/TimeControl.php
+++ htdocs/lib/class/TimeControl.php
@@ -4,13 +4,21 @@
 	
 	class TimeControl
 	{
+		private $connection;
 		public __construct()
 		{
-			
+			$this->connection = new Database();
 		}
 		
+		/**
+		* 取得學習的總時間
+		* @return		學習的總時間
+		*/
 		public getLearnTotalTime()
 		{
-			
+			$result = $this->connection->prepare("SELECT Theme_LearnTotal FROM ".$this->connection->table("theme"));
+			$result->execute();
+			$row = $result->fetch();
+			return $row["Theme_LearnTotal"];
 		}
 	}
