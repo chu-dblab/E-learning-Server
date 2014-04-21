d16ffdd3d50bd8d67d3af8d1c1a684c8b028f6f7
Yuan Chiu <me@yuaner.tw>
2014-04-21 15:52:08 +0800

    Prepare Fix Code Struct

---
+++ htdocs/lib/class/LearnNode.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * 推薦學習點類別
+ */
+ 
+  //前置作業
+  require_once("../../../lib/include.php");
+  require_once(DOCUMENT_ROOT."lib/class/Database.php");
+
+// ==================================================================
+/**
+ * 學習點類別
+ * 
+ * 這個物件代表一個學習點
+ * TODO 準備取代RecommandLearnNode類別用
+ *  
+ */
+class LearnNode {
+
+	/**
+	 * 標的編號
+	 *
+	 * 對應target資料表的TID欄位
+	 *
+	 * @access private
+	 * @var int
+	 */
+	private $tID;
+
+	// ----------------------------------------------------------
+	/**
+	 * 建構子
+	 *
+	 * @param int $tID 標的編號
+	 */
+	public function __construct($tID){
+
+		$this->tID = $tID;
+	}
+
+	// ----------------------------------------------------------
+	/**
+	 * 取得目前在此學習點有多少人
+	 *
+	 * @access public
+	 * @return int 目前此點有多少人
+	 * 
+	 */
+	public function getCurrentPeopleNum() {
+
+	}
+
+	/**
+	 * 取得在此學習點能容納人數
+	 *
+	 * @access public
+	 * @return int 此點可容納多少人
+	 * 
+	 */
+	public function getMaxPeopleNum() {
+
+	}		
+
+	// ----------------------------------------------------------
+	/**
+	 * 目前是否沒人
+	 *
+	 * @access public
+	 * @return boolean 目前有沒有人
+	 * 
+	 */
+	public function isNoPeople() {
+
+	}	
+
+	/**
+	 * 目前是否已經額滿
+	 *
+	 * @access public
+	 * @return boolean 目前人數是否已滿
+	 * 
+	 */
+	public function isFullPeople() {
+
+	}
+
+	 // ----------------------------------------------------------
+	/**
+	 * 加人數
+	 *
+	 * 當使用者的手機偵測到NFC Tag或掃描到QR code, 則人數加一
+	 *
+	 * @access private
+	 */
+	public function addCurrentPeople() {
+
+	}
+
+	/**
+	 * 減人數
+	 *
+	 * 當使用者按下"離開"按鈕時, 則人數減一
+	 *
+	 * @access public
+	 */
+	public function subCurrentPeople() {
+
+	}
+}
---
+++ htdocs/lib/class/LearnRecommand.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * 推薦學習點類別
+ */
+ 
+  //前置作業
+  require_once("../../../lib/include.php");
+  require_once(DOCUMENT_ROOT."lib/class/Database.php");
+
+  // ===============================================================
+/**
+ * 推薦學習點類別
+ * 
+ * 這個物件代表一個學習點
+ * TODO 準備取代RecommandLearnNode類別用
+ *  
+ */
+class LearnRecommand {
+
+	/**
+	 * 使用者物件
+	 *
+	 * @access private
+	 * @var MyUser Object
+	 *
+	 */
+	private $user;
+
+	/**
+	 * 調和參數
+	 *
+	 * 此欄位是常數值
+	 * 
+	 * @access private
+	 * @var int
+	 */
+	const ALPHA = 0.5; //調和參數
+
+	/**
+	 * 正規化參數
+	 *
+	 * @access private
+	 * @var double
+	 */
+	private $gamma;  //正規化參數
+
+	// -------------------------------------------------------------
+
+	/**
+	* getLearningNode
+	*
+	* 取得學習點的參數值，將數值帶入公式計算出推薦分數最高的前三名
+	* TODO 我沒看懂，待修
+	*
+	* @param int 目前剩餘學習時間
+	* @return $recommand array 系統推薦的學習點 
+	*/
+	public function getLearningNode($remainingTime) {
+
+	}
+
+	/**
+	 * 所有學習點是否已學完
+	 *
+	 * 確認學習點是否已經學完了
+	 * TODO 為什麼要帶那麼多參數？
+	 * @param array $matrix 下一個學習點(含虛擬點)
+	 * @param int $point_number 標的編號
+	 * @param int $remainingTime 目前剩餘學習時間
+	 * @return 系統推薦的學習點(已過濾)
+	 */
+	private function checkIsAllPointAreLearned($matrix
+	                                           ,$point_number
+	                                           ,$remainingTime) {
+
+	}
+
+	/**
+	 * computeNormalizationParam
+	 *
+	 * 計算正規化參數
+	 *
+	 * @return 正規化參數
+	 */
+	public function computeNormalizationParam() {
+
+	}
+
+	/**
+	* getNodeOfLearnOfParameter
+	*
+	* 取得學習點的所有參數
+	* TODO 這是什麼？
+	*
+	* @param int $next_point_number 學習點的編號
+	* @return 學習點之所有參數(array)
+	*/	
+	private function getNodeOfLearnOfParameter($next_point_number) {
+
+	}
+
+	// -------------------------------------------------------------
+	/**
+	 * getLearningStatus
+	 * 
+	 * 取得使用者學習的狀態
+	 *
+	 * @param int 學習點的編號
+	 * @return 學習狀態資訊(array)
+	 */	
+	public function getLearningStatus($point_number) {
+		
+	}
+	
+	/**
+	 * checkFinish
+	 *
+	 * 確認使用者是不是學過系統推薦的學習點
+	 *
+	 * @param int 學習點編號
+	 * @return boolean true->已經學過系統推薦的學習點
+	 *                 ,false->還沒學過系統推薦的學習點
+	 */
+	private function checkFinish($point) {
+
+	}
+
+}
